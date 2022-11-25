f43a751e1232eadf602afd522b112b0048b97d42
s-sakib-ahmed <you@example.com>
2022-11-26 02:26:57 +0600

    Add Ecommerce page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,19 @@
 import React from "react"
 
-const Button = () => {
-  return <div>Button</div>
+const Button = ({ color, bgColor, text, size, borderRadius, onClick }) => {
+  return (
+    <button
+      type="button"
+      className={`text-${size || "md"} p-3 hover:drop-shadow-xl`}
+      style={{
+        backgroundColor: bgColor,
+        color: color,
+        borderRadius: borderRadius,
+      }}
+    >
+      {text}
+    </button>
+  )
 }
 
 export default Button
